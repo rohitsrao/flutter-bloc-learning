61b079364688d39320dd4540a1c380e3378178b4
Rohit Rao <rohit@rohitsrao.com>
2022-03-27 14:32:13 +0530

    Tested cubits using bloc_test package and also Equatable

---
+++ bloc-testing/lib/cubit/counter_state.dart
@@ -0,0 +1,15 @@
+part of 'counter_cubit.dart';
+
+class CounterState extends Equatable {
+
+  final int counterValue;
+  final bool wasIncremented;
+
+  const CounterState({
+    required this.counterValue,
+    required this.wasIncremented,
+  });
+
+  @override
+  List<Object> get props => [counterValue, wasIncremented];
+}
---
+++ bloc-testing/lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cubit/counter_cubit.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      )
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+ 
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.wasIncremented == true) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Incremented'),
+                duration: Duration(milliseconds: 300),
+              ),
+            );
+          }
+          else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Decremented'),
+                duration: Duration(milliseconds: 300),
+              )
+            );
+          }
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              BlocBuilder<CounterCubit, CounterState>(
+                builder: (context, state) {
+                  return Text(
+                    '${state.counterValue}',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                      //context.bloc<CounterCubit>().decrement();
+                    },
+                    tooltip: 'Decrement',
+                    child: const Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                      //context.bloc<CounterCubit>().increment();
+                    },
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                ]
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
