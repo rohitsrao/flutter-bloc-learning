7da8b2facfb899ae3efbfe6cc143bce3e6d25402
Rohit Rao <rohit@rohitsrao.com>
2022-03-30 09:59:13 +0530

    Created anonymous route access with BlocProvider.value

---
+++ bloc-provider-anonymous-route-access/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloctest/logic/cubit/counter_cubit.dart';
+import 'package:bloctest/presentation/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BlocProvider<CounterCubit>(
+        create: (context) => CounterCubit(),
+        child: const HomeScreen(
+          title: 'Flutter Demo Home Page',
+          color: Colors.blueAccent,
+        ),
+      ),
+    );
+  }
+}
+
